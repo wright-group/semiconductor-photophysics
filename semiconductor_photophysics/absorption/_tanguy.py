8f872025029045b076b431476fabd40ccfbe105d
Darien Morrow <darienmorrow@gmail.com>
2018-01-17 18:20:38 -0600

    [WIP] 2D dielectric. NEEDS TESTING

--- semiconductor_photophysics/absorption/_tanguy.py
+++ semiconductor_photophysics/absorption/_tanguy.py
@@ -24,31 +24,39 @@ def xi(z, Eg, R):
     """
     return np.sqrt(R/(Eg-z))
 
-def g(z, Eg, R, allowed=True):
+def g(z, Eg, R, allowed=True, three_D=True):
     """
     """
     xi_ = xi(z, Eg, R) # only evalute once
-    ga = 2*np.log(xi_) - 2*np.pi*(np.cos(np.pi*xi_)/np.sin(np.pi*xi_)) - 2*psi(xi_) - 1/xi_
-    if allowed:
-        return ga
+    if three_D:
+        ga = 2*np.log(xi_) - 2*np.pi*(np.cos(np.pi*xi_)/np.sin(np.pi*xi_)) - 2*psi(xi_) - 1/xi_
+        if allowed:
+            return ga
+        else:
+            gf = (z - Eg + R)*ga
+            return gf
     else:
-        gf = (z - Eg + R)*ga
-        return gf
-
-def e_G(E, Eg, R, G, A, allowed=True):
+        ga = 2*np.log(xi_) - 2*psi(0.5-xi_)
+        if allowed:
+            return ga
+        else:
+            gf = (z - Eg + 4*R)*ga
+            return gf
+        
+def e_G(E, Eg, R, G, A, allowed=True, three_D=True):
     """
     """
     z = E + 1j*G
-    prefactor = A * np.sqrt(R) / (z)**2
-    postfactor = g(z, Eg, R, allowed=allowed) 
-    postfactor += g(-1*z, Eg, R, allowed=allowed) 
-    postfactor -= 2 * g(0, Eg, R, allowed=allowed)
+    prefactor = A * np.sqrt(R) / (z)**2 / np.pi
+    postfactor = g(z, Eg, R, allowed=allowed, three_D=three_D) 
+    postfactor += g(-1*z, Eg, R, allowed=allowed, three_D=three_D) 
+    postfactor -= 2 * g(0, Eg, R, allowed=allowed, three_D=three_D)
     return prefactor * postfactor
 
-def e_both(E, Eg, R, G, A_allowed, A_forbidden):
+def e_both(E, Eg, R, G, A_allowed, A_forbidden, three_D=True):
     """
     """
-    return e_G(E, Eg, R, G, A_allowed, allowed=True) + e_G(E, Eg, R, G, A_forbidden, allowed=False)
+    return e_G(E, Eg, R, G, A_allowed, allowed=True, three_D=three_D) + e_G(E, Eg, R, G, A_forbidden, allowed=False, three_D=three_D)
 
 def complex_index(e):
     """
@@ -60,11 +68,11 @@ def complex_index(e):
     return n, k
 
 x = np.linspace(1.5,2.5,1000)
-y = e_both(x, 2.4, .5, .05, 4, 2) + e_both(x, 2.4, .4, .05, 4, 2)
-n, k = complex_index(y)
-#y = e_G(x, 2, .1, .01, 1, allowed=False)
-#plt.plot(x, y.real)
-#plt.plot(x, y.imag)
+y3 = e_both(x, 2.4, .5, .05, 4, 2, three_D=True) + e_both(x, 2.4, .4, .05, 4, 2, three_D=True)
+y2 = e_both(x, 2.4, .5, .05, 4, 2, three_D=False)# + e_both(x, 2.4, .4, .05, 4, 2, three_D=False)
+# need to test 2D. Reproduce results of  Tanguy's paper.
+plt.plot(x, y3.real)
+plt.plot(x, y3.imag)
 
-plt.plot(x, n)
-plt.plot(x, k)
+#plt.plot(x, y2.real)
+plt.plot(x, y2.imag)
